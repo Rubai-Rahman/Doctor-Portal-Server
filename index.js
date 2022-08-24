ef1b74637cbdaae15f4b7aa5c7d44d5d27f1d63e
Rubai-Rahman <rubairahman1@gmail.com>
2022-08-24 22:57:57 +0600

    connced with frontEnd

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require("mongodb");
 app.use(cors());
 require("dotenv").config();
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.f8dsb.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -16,7 +17,25 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    console.log("database connected successfully");
+    const database = client.db("doctors_portal");
+    const appointmentsCollection = database.collection("appointments");
+
+    app.post("/appointments", async (req, res) => {
+      const appointment = req.body;
+      const result = await appointmentsCollection.insertOne(appointment);
+      console.log(appointment);
+      res.json(result);
+    });
+    //get//
+    app.get("/appointments", async (req, res) => {
+      const email = req.query.email;
+      const date = new Date(req.query.date).toLocaleDateString();
+      console.log(date)
+      const query = {email: email,date:date };
+      const cursor = appointmentsCollection.find(query );
+      const appointments = await cursor.toArray();
+      res.json(appointments);
+    });
   } finally {
     //await client.close();
   }
